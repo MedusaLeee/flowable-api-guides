c811acb4fb989c29f162f064c1d2c09c425a79de
jianxun <medusa.leee@gmail.com>
2018-03-22 15:39:50 +0800

    增加获取历史评论的方法

--- src/main/java/com/viathink/flowable/Part4.java
+++ src/main/java/com/viathink/flowable/Part4.java
@@ -1,15 +1,18 @@
 package com.viathink.flowable;
 
 import org.flowable.engine.*;
+import org.flowable.engine.history.HistoricActivityInstance;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.repository.ProcessDefinitionQuery;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.task.Comment;
 import org.flowable.task.api.Task;
+import org.flowable.task.api.history.HistoricTaskInstance;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,7 +62,7 @@ public class Part4 {
         // 部署
         repositoryService
             .createDeployment()
-            .name("监听器流程")
+            .name("监听器流程-0322")
             .addInputStream("listener.bpmn", in)
             .deploy();
         // 验证部署是否部署成功
@@ -121,12 +124,13 @@ public class Part4 {
     }
     @Test
     public void completeTaskWithComment() {
-        String taskId = "52502";
+        String taskId = "62504";
         String userId = "lijianxun";
         IdentityService identityService = processEngine.getIdentityService();
         identityService.setAuthenticatedUserId(userId);
         TaskService taskService = processEngine.getTaskService();
         Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        System.out.println("processInstanceId: " + task.getProcessInstanceId());
         taskService.addComment(taskId, task.getProcessInstanceId(), "同意，审批通过");
         // 对于执行完的任务，activiti将从act_ru_task表中删除该任务，下一个任务会被插入进来
         taskService.complete(taskId);
@@ -134,6 +138,32 @@ public class Part4 {
         List<Comment> taskComments = taskService.getProcessInstanceComments(task.getProcessInstanceId());
         for(Comment c: taskComments) {
             System.out.printf("任务: %s, 审批人：%s， 审批意见: %s", c.getTaskId(), c.getUserId(), c.getFullMessage());
+        }
+    }
+    // 根据目前任务的id去查找此任务同一个流程实例的历史评论 #246
+    @Test
+    public void getProcessHistoryComments() {
+        String taskId = "62504";
+        TaskService taskService = processEngine.getTaskService();
+        HistoryService historyService = processEngine.getHistoryService();
+        // 通过task获取流程实例的id
+        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        String processInstanceId = task.getProcessInstanceId();
+        System.out.println("processInstanceId: " + processInstanceId);
+        // 通过流程实例查询所有的(用户任务类型)历史活动, 也可以查活动的 # http://www.cnblogs.com/cxyj/p/3898535.html
+        List<Comment> historyCommnets = new ArrayList<Comment>();
+        List<HistoricActivityInstance> hais = historyService.createHistoricActivityInstanceQuery().processInstanceId(processInstanceId).activityType("userTask").list();
+//       3）查询每个历史任务的批注
+        for (HistoricActivityInstance hai : hais) {
+            String historytaskId = hai.getTaskId();
+            List<Comment> comments = taskService.getTaskComments(historytaskId);
+            // 4）如果当前任务有批注信息，添加到集合中
+            if(comments!=null && comments.size()>0){
+                historyCommnets.addAll(comments);
+            }
+        }
+        for (Comment c: historyCommnets) {
+            System.out.println("comment: " + c.getId() + ",userId: " + c.getUserId() + ",message: " + c.getFullMessage());
         }
     }
 }
